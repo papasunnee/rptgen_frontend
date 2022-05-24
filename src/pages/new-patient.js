40bc000f69f25da5f697eac4f71a48a4a782b306
papasunnee <papasunnee@users.noreply.github.com>
2022-05-24 14:09:00 +0100

    update pages

--- src/pages/new-patient.js
+++ src/pages/new-patient.js
@@ -1,9 +1,7 @@
 import React, { Fragment } from "react";
 import Frame47 from "@/components/Frame47/Frame47";
-import { useAuth } from "@/hooks__/auth";
 
 function NewPatient() {
-  useAuth({ middleware: "auth" });
   return (
     <Fragment>
       <Frame47 />
