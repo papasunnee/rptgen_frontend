33bbd268f744dabd9bfeb8254bc4c645c907ca3b
papasunnee <papasunnee@users.noreply.github.com>
2022-08-25 02:58:58 +0100

    update

--- src/components/Modals/MedicalhistoryformModals/TypeofworkTrigger.js
+++ src/components/Modals/MedicalhistoryformModals/TypeofworkTrigger.js
@@ -13,77 +13,92 @@ import { fetcher } from "@/context/AuthContext";
 import { UserContext } from "@/context/UserContext";
 
 function TypeofworkTrigger() {
-    const [modalShow, setModalShow] = React.useState(false);
-    return (
-        <>
-            <Button
-                variant="primary"
-                onClick={() => setModalShow(true)}
-                className={`${frame44Styles.Selectinput} col-md-3`}
-            >
-                <input
-                    type="text"
-                    placeholder="Eg. your text here"
-                    name="physical_activity"
-                    style={{ width: "90%" }}
-                />
-            </Button>
+  const [modalShow, setModalShow] = useState(false);
+  return (
+    <>
+      <Button
+        variant="primary"
+        onClick={() => setModalShow(true)}
+        className={`${frame44Styles.Selectinput} col-md-3`}
+      >
+        <input
+          type="text"
+          placeholder="Eg. your text here"
+          name="physical_activity"
+          style={{ width: "90%" }}
+        />
+      </Button>
 
-            <TypeofworkModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                setModalShow={setModalShow}
-            />
-        </>
-    );
+      <TypeofworkModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        setModalShow={setModalShow}
+      />
+    </>
+  );
 }
 
 export default TypeofworkTrigger;
 
 function TypeofworkModal(props) {
-    return (
-        <Modal
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            className={`${functionalStyles.Modal}`}
+  const [selectedWork, setSelectedWork] = useState([]);
+  const handleClick = (item) => {
+    const hasClass = document
+      .getElementById(item.name)
+      .classList.contains("Functionalimprovement_activeSelection___SGsl");
+    if (hasClass) {
+      document
+        .getElementById(item.name)
+        .classList.remove("Functionalimprovement_activeSelection___SGsl");
+      //   const filtered = selectedWork.filter((work) => item.name !== work.name);
+      //   setSelectedWork([...filtered]);
+    } else {
+      document
+        .getElementById(item.name)
+        .classList.add("Functionalimprovement_activeSelection___SGsl");
+      //   setSelectedWork([...selectedWork, item]);
+    }
+  };
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className={`${functionalStyles.Modal}`}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title
+          id="contained-modal-title-vcenter"
+          className={`${functionalStyles.Modal_title}`}
         >
-            <Modal.Header closeButton>
-                <Modal.Title
-                    id="contained-modal-title-vcenter"
-                    className={`${functionalStyles.Modal_title}`}
-                >
-                    Select Type of Work
-                </Modal.Title>
-            </Modal.Header>
-            <form>
-                <Modal.Body className={`${functionalStyles.Modal_con}`}>
-
-                    <div className={`${functionalStyles.Selectitems_con}`}>
-                        <button className={`${functionalStyles.Selectitems}`}>
-                            Auto
-                        </button>
-
-                        <button className={`${functionalStyles.Selectitems}`}>
-                            Home
-                        </button>
-
-                        <button className={`${functionalStyles.Selectitems}`}>
-                            Sports
-                        </button>
-
-                        <button className={`${functionalStyles.Selectitems}`}>
-                            Work
-                        </button>
-
-                        <button className={`${functionalStyles.Selectitems}`}>
-                            Other
-                        </button>
-                    </div>
-
-                </Modal.Body>
-            </form>
-        </Modal>
-    );
+          Select Type of Work
+        </Modal.Title>
+      </Modal.Header>
+      <form>
+        <Modal.Body className={`${functionalStyles.Modal_con}`}>
+          <div className={`${functionalStyles.Selectitems_con}`}>
+            {WorkType.map((item, index) => (
+              <div
+                onClick={() => handleClick(item)}
+                key={index}
+                id={item.name}
+                className={`${functionalStyles.Selectitems}`}
+              >
+                {item.name}
+              </div>
+            ))}
+          </div>
+        </Modal.Body>
+      </form>
+    </Modal>
+  );
 }
+
+const WorkType = [
+  { name: "Auto" },
+  { name: "Home" },
+  { name: "Sports" },
+  { name: "Work" },
+  { name: "Others" },
+];
--- src/pages/api/admin/users/index.js
+++ src/pages/api/admin/users/index.js
@@ -4,48 +4,52 @@ import User from "@/models/User";
 
 export default async function handler(req, res) {
   const { method } = req;
+  try {
+    await dbConnect();
 
-  await dbConnect();
-
-  switch (method) {
-    case "GET":
-      try {
-        let users;
-        let id = req?.query?.id;
-        if (id && (id != "undefined" || id != null || id != "null")) {
-          users = await User.findOne(
-            {
-              _id: id,
-            },
-            {
-              password: false,
-            }
-          ).populate("payments"); /* find all the data in our database */
-        } else {
-          users = await User.find(
-            {
-              $or: [{ isAdmin: { $ne: true } }, { isAdmin: null }],
-            },
-            {
-              password: false,
-            }
-          ).populate("payments"); /* find all the data in our database */
+    switch (method) {
+      case "GET":
+        try {
+          let users;
+          let id = req?.query?.id;
+          if (id && (id != "undefined" || id != null || id != "null")) {
+            users = await User.findOne(
+              {
+                _id: id,
+              },
+              {
+                password: false,
+              }
+            ).populate("payments"); /* find all the data in our database */
+          } else {
+            users = await User.find(
+              {
+                $or: [{ isAdmin: { $ne: true } }, { isAdmin: null }],
+              },
+              {
+                password: false,
+              }
+            ).populate("payments"); /* find all the data in our database */
+          }
+          return res.status(200).json({ success: true, data: users });
+        } catch (error) {
+          return res.status(400).json({ success: false, error: error.message });
         }
-        return res.status(200).json({ success: true, data: users });
-      } catch (error) {
-        return res.status(400).json({ success: false, error: error.message });
-      }
-    case "POST":
-      try {
-        const user = await User.create(
-          req.body
-        ); /* create a new model in the database */
-        return res.status(201).json({ success: true, data: user });
-      } catch (error) {
-        return res.status(400).json({ success: false });
-      }
-    default:
-      res.status(400).json({ success: false });
-      break;
+      case "POST":
+        try {
+          const user = await User.create(
+            req.body
+          ); /* create a new model in the database */
+          return res.status(201).json({ success: true, data: user });
+        } catch (error) {
+          return res.status(400).json({ success: false });
+        }
+      default:
+        res.status(400).json({ success: false });
+        break;
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: "Network Error" });
   }
 }
